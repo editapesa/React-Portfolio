e2a2c2ae0f5809d2783ad1ad08f8b7c2ad4da7fb
Edita Pesa <editapesa@gmail.com>
2021-06-24 17:14:07 -0400

    add code for Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <p>
+            NYC based web developer, with a background in management, hospitality, and accounting. Ready for a new journey in full stack development, utlizing skills such as HTML, CSS, JavaScript, MongoDB, Express, React, and Node.js. 
+            </p>
+        </div>
+    );
+}
+
+export default Home;
