99768499c469bb7d40ed8c20151a942fb8c72d44
Edita Pesa <editapesa@gmail.com>
2021-06-24 16:54:50 -0400

    add code to Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+//import style css
+//import picOfMe from '../'
+
+function Header() {
+    return (
+        <div className='header text-left'>
+            <h1>Edita Pesa</h1>
+            {/* <img src={picOfMe} alt='Edita pic' /> */}
+        </div>
+    );
+}
+
+export default Header;
