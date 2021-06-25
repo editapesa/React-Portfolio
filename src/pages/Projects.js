2e7133461477e39f6a6deda94768cb3958106745
Edita Pesa <editapesa@gmail.com>
2021-06-24 22:31:13 -0400

    add code for Projects.js

---
+++ src/pages/Projects.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import ProjectCard from '../components/ProjectCard'
+import projects from '../projects.json';
+
+// function Projects() 
+
+class Projects extends Component {
+    state = {
+        projects
+    };
+
+    render() {
+        return (
+            <div>
+            <h1 className='text-center p-2'>Projects</h1> 
+                {this.state.projects.map(project => (
+                    <ProjectCard
+                    id={project.id}
+                    key={project.id}
+                    title={project.title}
+                    image={project.image}
+                    github={project.github}
+                    url={project.url}
+                    />
+                ))}
+            </div>
+        );
+    }
+}
+
+export default Projects;
